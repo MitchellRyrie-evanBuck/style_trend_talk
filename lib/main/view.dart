54e74e03c033ad347b10cf35b07a6f8e8d88cd56
af-liu <18264480900@163.com>
2023-09-24 21:53:22 +0800

    add: getX

---
+++ lib/main/view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'index.dart';
+
+class MainPage extends GetView<MainController> {
+  const MainPage({Key? key}) : super(key: key);
+
+  // 主视图
+  Widget _buildView() {
+    return const Center(
+      child: Text("MainPage"),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<MainController>(
+      init: MainController(),
+      id: "main",
+      builder: (_) {
+        return Scaffold(
+          appBar: AppBar(title: const Text("main")),
+          body: SafeArea(
+            child: _buildView(),
+          ),
+        );
+      },
+    );
+  }
+}
