aba9eaf9ca916190409b53658ef1f6dc0820aa8e
af-liu <18264480900@163.com>
2023-10-12 16:56:44 +0800

    feat: 暂存

--- lib/data/models/mock/recommendation.dart
+++ lib/data/models/mock/recommendation.dart
@@ -41,4 +41,25 @@ List<Map<String, dynamic>> recommendationList = [
         'Life was like a box of chocolates, you never know what you going to get.o',
     '@': []
   },
+  {
+    'user_name': 'Anfu... Tom',
+    'user_id': 41232,
+    'flow': true,
+    'flow_self': true,
+    'desc':
+        'Life was like a box of chocolates, you never know what you going to get.',
+    'like': 123,
+    'like_self': true,
+    'star': 234,
+    'star_self': false,
+    'share': 987,
+    'create_timer': '2023-10-10',
+    'update_timer': '2023-10-10',
+    'type': 1, // 1 ,2, 3
+    'vedio': null,
+    'photo': [],
+    'text':
+        'Life was like a box of chocolates, you never know what you going to get.o',
+    '@': []
+  },
 ];
