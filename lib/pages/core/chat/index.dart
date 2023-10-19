7ac8db4383dec954cb93a844d285c88ad9ffe3ee
af-liu <18264480900@163.com>
2023-10-19 15:35:03 +0800

    feat: .

---
+++ lib/pages/core/chat/index.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatPages extends StatefulWidget {
+  const ChatPages({super.key});
+
+  @override
+  State<ChatPages> createState() => _ChatPagesState();
+}
+
+class _ChatPagesState extends State<ChatPages> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
