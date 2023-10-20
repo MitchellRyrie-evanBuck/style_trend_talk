77167a42adaed567abaae02d227ab8de2456a6a7
af-liu <18264480900@163.com>
2023-10-20 11:22:42 +0800

    feat: /.

---
+++ lib/pages/core/userDetails/user_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserDetailsPages extends StatefulWidget {
+  const UserDetailsPages({super.key});
+
+  @override
+  State<UserDetailsPages> createState() => _UserDetailsPagesState();
+}
+
+class _UserDetailsPagesState extends State<UserDetailsPages> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
