3cfb0362f2f62d950e7cf4c7d93496329faaf4a2
af-liu <18264480900@163.com>
2023-11-15 22:57:39 +0800

    feat: 视频 控件

---
+++ lib/pages/discover/widgets/comment/comment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CommentWidget extends StatefulWidget {
+  const CommentWidget({super.key});
+
+  @override
+  State<CommentWidget> createState() => _CommentWidgetState();
+}
+
+class _CommentWidgetState extends State<CommentWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/discover/widgets/share/share.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShareWidget extends StatefulWidget {
+  const ShareWidget({super.key});
+
+  @override
+  State<ShareWidget> createState() => _ShareWidgetState();
+}
+
+class _ShareWidgetState extends State<ShareWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
