211e4117016357c6a7540843ed5e81a46c57a074
af-liu <18264480900@163.com>
2023-11-24 23:50:49 +0800

    feat: 暂存

--- lib/widget/videoWidget.dart
+++ lib/widget/videoWidget.dart
@@ -2,6 +2,8 @@
 // ignore: file_names
 // ignore_for_file: avoid_print, file_names
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:style_trend_talk/data/fitness_app_theme.dart';
@@ -26,12 +28,17 @@ class _VideoComponentState extends State<VideoComponent>
   double _videoHeight = 220.0; // 初始高度，可以根据需要调整
   final double _maxVideoHeight = 400.0; // 最大高度，可以根据需要调整
   double _videoWidth = 300;
-  double iconSize = 50;
+  double iconSize = 30;
   late bool isPlaying = false;
   late Duration duration;
   late Duration position;
   late bool showControls;
-  final Duration animatedPlayFlag = const Duration(milliseconds: 500);
+  final Duration milliseconds300 = const Duration(milliseconds: 300);
+  final Duration seconds2 = const Duration(seconds: 2);
+  late Timer delayedTimer = Timer(milliseconds300, () {});
+
+  /// 每次执行完 暂停 播放时 都会置为false
+  bool operation = true; // 检测你在 两秒内 有没有动作
   late Animation<double> _animation;
   late AnimationController _animationController;
   bool isVisibility = true;
@@ -66,7 +73,7 @@ class _VideoComponentState extends State<VideoComponent>
 
   _initPlayController() {
     _animationController = AnimationController(
-      duration: animatedPlayFlag,
+      duration: milliseconds300,
       vsync: this,
     );
 
@@ -110,6 +117,8 @@ class _VideoComponentState extends State<VideoComponent>
       3.蒙板显示开始播放按钮 并于一秒后 慢慢消失
       4.暂停与播放 有一个变换的动画
       5.播放完成之后 显示蒙版 和 开始播放按钮
+      6.滑动到当前视区时 自动开始根据设置的 声音 开始静音/有音 播放
+      7. 播放完开始循环播放
     */
     _controller.addListener(() {
       // 在此可以处理视频播放状态的变化
@@ -131,42 +140,91 @@ class _VideoComponentState extends State<VideoComponent>
     });
 
     _controller.addListener(() {
-      // 监听点击完开始之后 慢慢消失icon按钮
-      if (_controller.value.isPlaying) {
-        // setState(() {
-        //   showControls = false;
-        //   _animationController.forward();
-        //   Future.delayed(animatedPlayFlag, () {
-        //     isVisibility = false;
-        //   });
-        // });
-      }
-      // if (!_controller.value.isPlaying) {
-      //   setState(() {
-      //     showControls = true;
-      //     _animationController.reverse();
-      //   });
-      // }
-
       // 播放完成之后 索引回初
       if (!_controller.value.isPlaying &&
           _controller.value.position == duration) {
         _controller.seekTo(Duration.zero); // Reset to the beginning
         _controller.pause();
+        setState(() {
+          showControls = true;
+          isVisibility = true;
+          _animationController.reverse();
+        });
       }
     });
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
-    _animationController.dispose();
-    // chewieController.dispose();
+  void _displayContoller() {
+    setState(() {
+      showControls = false;
+      _animationController.forward();
+      Future.delayed(milliseconds300, () {
+        isVisibility = false;
+        operation = true;
+      });
+    });
   }
 
-  @override
-  bool get wantKeepAlive => true; // 启用状态保持
+  void _showPlayController() {
+    setState(() {
+      showControls = true;
+      _animationController.reverse();
+      isVisibility = true;
+      // TODO 展开蒙层之后没有下一步动作 还是会继续隐藏蒙层
+      delayedTimer = Timer(seconds2, () {
+        if (operation) {
+          // true 没有进行操作
+          // 隐藏控制栏
+          _displayContoller();
+        }
+      });
+    });
+  }
+
+  /// 点击开始播放之后 自动 [_displayContoller] 隐藏控制窗
+  void iconCallback() {
+    setState(() {
+      print('出发play事件showControls---${showControls}');
+      print('出发play事件operation---${operation}');
+
+      // if (!showControls) {
+      //   _animationController.reverse();
+      //   setState(() {
+      //     showControls = true;
+      //   });
+      //   return;
+      // }
+
+      if (isPlaying) {
+        _controller.pause();
+        delayedTimer.cancel();
+        _displayContoller();
+        // delayedTimer = Timer(seconds2, () {
+        //   if (operation) {
+        //   }
+        // });
+      } else {
+        delayedTimer.cancel();
+        _displayContoller();
+        _controller.play();
+      }
+      operation = false;
+    });
+  }
+
+  /// 最外层GestureDetector 点击生效
+  /// 点击显示蒙层 唯一入口
+  _viewsContainerCallback() {
+    setState(() {
+      print('最外层GestureDetector--${showControls}');
+      if (!showControls) {
+        _showPlayController();
+      }
+      if (showControls) {
+        showControls = false;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -174,20 +232,9 @@ class _VideoComponentState extends State<VideoComponent>
     return isShow
         ? GestureDetector(
             onTap: () {
-              setState(() {
-                print('最外层GestureDetector--${showControls}');
-                // showControls = true;
-                if (!showControls) {
-                  setState(() {
-                    showControls = true;
-                    isVisibility = true;
-                  });
-                  _animationController.reverse();
-                  return;
-                }
-              });
+              _viewsContainerCallback();
             },
-            behavior: HitTestBehavior.translucent,
+            behavior: HitTestBehavior.deferToChild,
             child: Container(
                 height: _videoHeight,
                 width: MediaQuery.of(context).size.width,
@@ -220,62 +267,43 @@ class _VideoComponentState extends State<VideoComponent>
             child: FadeTransition(
           opacity: _animation,
           child: AnimatedContainer(
-              duration: const Duration(milliseconds: 500),
-              color: Color.fromARGB(69, 0, 0, 0)),
+              duration: milliseconds300,
+              color: const Color.fromARGB(69, 0, 0, 0)),
         )),
         Positioned.fill(
           child: FadeTransition(
             opacity: _animation,
             child: AnimatedContainer(
-              duration: const Duration(milliseconds: 500),
+              duration: milliseconds300,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Visibility(
                     visible: isVisibility,
                     child: AnimatedSwitcher(
-                      duration: animatedPlayFlag,
-                      transitionBuilder: (child, animation) {
-                        return ScaleTransition(
-                          scale: animation,
-                          child: child,
-                        );
-                      },
-                      child: IconButton(
-                        key: ValueKey<bool>(isPlaying),
-                        onPressed: () {
-                          setState(() {
-                            print('出发play事件${showControls}');
-                            if (!showControls) {
-                              _animationController.reverse();
-                              setState(() {
-                                showControls = true;
-                              });
-                              return;
-                            }
-                            if (isPlaying) {
-                              _controller.pause();
-                            } else {
-                              setState(() {
-                                showControls = false;
-                                _animationController.forward();
-                                Future.delayed(animatedPlayFlag, () {
-                                  isVisibility = false;
-                                });
-                              });
-                              _controller.play();
-                            }
-                          });
+                        duration: milliseconds300,
+                        transitionBuilder: (child, animation) {
+                          return ScaleTransition(
+                            scale: animation,
+                            child: child,
+                          );
                         },
-                        icon: Icon(
-                          isPlaying
-                              ? FontAwesomeIcons.pause
-                              : FontAwesomeIcons.play,
-                          size: iconSize,
-                          color: FitnessAppTheme.white,
-                        ),
-                      ),
-                    ),
+                        child: AbsorbPointer(
+                          absorbing: false,
+                          child: IconButton(
+                            key: ValueKey<bool>(isPlaying),
+                            onPressed: () {
+                              iconCallback();
+                            },
+                            icon: Icon(
+                              isPlaying
+                                  ? FontAwesomeIcons.pause
+                                  : FontAwesomeIcons.play,
+                              size: iconSize,
+                              color: FitnessAppTheme.white,
+                            ),
+                          ),
+                        )),
                   ),
                 ],
               ),
@@ -296,4 +324,14 @@ class _VideoComponentState extends State<VideoComponent>
             child: ProcesssFlicker(),
           );
   }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+    _animationController.dispose();
+  }
+
+  @override
+  bool get wantKeepAlive => true; // 启用状态保持
 }
