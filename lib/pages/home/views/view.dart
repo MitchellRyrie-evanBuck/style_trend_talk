c0e43470b156479602b81038e6f650ace37bab32
af-liu <18264480900@163.com>
2023-11-27 21:45:44 +0800

    feat: ...

--- lib/pages/home/views/view.dart
+++ lib/pages/home/views/view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:style_trend_talk/data/fitness_app_theme.dart';
+// import 'package:style_trend_talk/data/fitness_app_theme.dart';
 // import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:style_trend_talk/data/tabIcon_data.dart';
 import 'package:style_trend_talk/layout/bottom_bar_view.dart';
