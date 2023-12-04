14b5ba3682f5d211c0e082bc02f4f3bd20b4b0a6
af-liu <18264480900@163.com>
2023-12-04 23:03:28 +0800

    feat: 输入框头部

--- lib/pages/core/search/widget/header.dart
+++ lib/pages/core/search/widget/header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
 // import 'package:get/get.dart';
 import 'package:style_trend_talk/data/fitness_app_theme.dart';
 // import 'package:style_trend_talk/pages/core/publish/controllers/publishControllers.dart';
@@ -30,42 +31,74 @@ class _SearchAppHeaderState extends State<SearchAppHeader>
 
 class MySliverPersistentHeaderDelegate extends SliverPersistentHeaderDelegate {
   MySliverPersistentHeaderDelegate();
+  TextEditingController _controller = TextEditingController();
 
   final double tabHeight = 48;
 
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
-    final double tabWidth = MediaQuery.of(context).size.width - 120;
+    final double tabWidth = MediaQuery.of(context).size.width - 60;
 
     return Container(
         color: Colors.white,
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            SizedBox(
+            Container(
+              width: 40,
+              padding: const EdgeInsets.only(left: 10),
               child: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    navigator?.pop(context);
+                  },
                   icon: const Icon(
                     FontAwesomeIcons.arrowLeft,
-                    size: 18,
+                    size: 20,
                   )),
             ),
-            const SizedBox(
-              child: Text(
-                '不期而遇',
-                style: TextStyle(
-                    color: FitnessAppTheme.black, fontWeight: FontWeight.w500),
-              ),
-            ),
-            SizedBox(
-              child: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    FontAwesomeIcons.bars,
-                    size: 18,
-                  )),
-            ),
+            Expanded(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                    width: tabWidth,
+                    height: 38,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25.0),
+                      color: const Color.fromARGB(
+                          255, 236, 236, 236), // Grey background color
+                    ),
+                    child: Center(
+                      child: TextField(
+                        controller: _controller,
+                        style: const TextStyle(color: Colors.black),
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: '请输入您的搜索关键词',
+                          hintStyle:
+                              const TextStyle(color: Colors.grey, fontSize: 14),
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 24.0, vertical: 0),
+                          suffixIcon: IconButton(
+                            icon: const Icon(
+                              FontAwesomeIcons.search,
+                              size: 18,
+                            ),
+                            onPressed: () {
+                              print('Searching for: ${_controller.text}');
+                            },
+                          ),
+                        ),
+                        onSubmitted: (text) {
+                          // Handle search action here as well
+                          print('Searching for: $text');
+                        },
+                      ),
+                    ))
+              ],
+            ))
           ],
         ));
   }
