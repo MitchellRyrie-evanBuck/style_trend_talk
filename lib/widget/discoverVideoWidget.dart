0d52bdd0476b8db1895fd2f5d248094b5baa97c1
af-liu <18264480900@163.com>
2023-12-22 22:41:54 +0800

    feat: 暂存

--- lib/widget/discoverVideoWidget.dart
+++ lib/widget/discoverVideoWidget.dart
@@ -5,7 +5,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:style_trend_talk/data/fitness_app_theme.dart';
 import 'package:style_trend_talk/widget/flickr.dart';
