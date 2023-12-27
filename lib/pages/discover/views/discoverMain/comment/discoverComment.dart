0139e38a114869f56a2cd561fbb9efd6e748f025
af-liu <18264480900@163.com>
2023-12-27 20:57:16 +0800

    feat: 视频评论

---
+++ lib/pages/discover/views/discoverMain/comment/discoverComment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DiscoverComment extends StatefulWidget {
+  const DiscoverComment({super.key});
+
+  @override
+  State<DiscoverComment> createState() => _DiscoverCommentState();
+}
+
+class _DiscoverCommentState extends State<DiscoverComment> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
