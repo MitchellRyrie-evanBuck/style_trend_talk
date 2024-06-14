ab8f9e755104f93c0e59b3b569ccffa32816695b
liuxiaowen <liuxiaowen66621@gmail.com>
2024-06-14 18:24:05 +0800

    pref: 暂存

--- lib/pages/profile/widgets/profileHeader.dart
+++ lib/pages/profile/widgets/profileHeader.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-// import 'package:get/get.dart';
 import 'package:style_trend_talk/data/fitness_app_theme.dart';
 
 class MyProfileSliverPersistentHeaderDelegate
