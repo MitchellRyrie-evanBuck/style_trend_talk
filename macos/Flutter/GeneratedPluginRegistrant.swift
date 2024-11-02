c0a99aa08c8ced436ffedb4f09e7b74775e6cdf4
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-11-02 23:50:33 +0800

    update: 依赖升级

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import video_player_avfoundation
 import wakelock_plus
 
